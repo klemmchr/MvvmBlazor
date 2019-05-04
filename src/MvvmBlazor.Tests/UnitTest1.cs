65214ba41594c06a7a9fd33ba3d65caf11042ca1
Christian Klemm <git@klemm.one>
2019-05-04 23:22:43 +0200

    ci adjusts

---
+++ src/MvvmBlazor.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace MvvmBlazor.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
