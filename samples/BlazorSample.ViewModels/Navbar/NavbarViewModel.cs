0a19f72cfcd098b526d248712b7db4e16028625e
Christian Klemm <git@klemm.one>
2022-02-11 21:19:40 +0100

    cleanup

--- samples/BlazorSample.ViewModels/Navbar/NavbarViewModel.cs
+++ samples/BlazorSample.ViewModels/Navbar/NavbarViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
 using BlazorSample.Domain.Services.Navbar;
 using Microsoft.AspNetCore.Components;
